538d5f30f27aedeee2f3642abf37e46e19848dec
carloeusebi <carloeusebi@gmail.com>
2023-06-01 15:39:11 +0200

    Layed down steps on javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,51 @@
-console.log('JSOK');
+// displayers
+const gallery = document.getElementById('gallery');
+const thumbnails = document.getElementById('thumbnails');
+
+// buttons
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+// to *pretend* we dinamically recieve path and #ofPictures from somewhere
+const path = 'img/'
+const numberOfPcitures = 5;
+
+const pictures = loadPicturesFromPath(path, numberOfPcitures);
+
+let currentIndex = 0;
+
+createElementsOnDom();
+
+displayPicture(currentIndex);
+
+prevButton.addEventListener('click', function () {
+
+    hidePicture(currentIndex);
+
+    //check if we are currently on the first image
+    currentIndex = currentIndex === 0 ? pictures.length : currentIndex--;
+
+    displayPicture(currentIndex);
+
+})
+
+nextButton.addEventListener('click', function () {
+
+    hidePicture(currentIndex);
+
+    //check if we are currently on the first image
+    currentIndex = currentIndex === pictures.length ? currentIndex = 0 : currentIndex++;
+
+    displayPicture(currentIndex);
+
+})
+
+
+function loadPicturesFromPath(path) { }
+
+function createElementsOnDom() { }
+
+function displayPicture(index) { }
+
+function hidePicture(inded) { }
+
